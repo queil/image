ebf3a4f01904d70eb4ffa84d8d4e1bd175a0d9bb
queil <queil@users.noreply.github.com>
2024-04-09 05:23:47 +0000

    add: more aliases

--- src/home/queil/.custom.sh
+++ src/home/queil/.custom.sh
@@ -3,6 +3,7 @@
 # ALIASES
 
 alias gr='cd $(git rev-parse --show-toplevel)'
+alias ga='git add'
 alias st='git status'
 alias di='git diff'
 alias co='git checkout'
@@ -10,6 +11,7 @@ alias cb='git checkout -b'
 alias cm='git commit -m'
 alias pu='git push -u origin "$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"'
 alias pl='git pull'
+alias uuid='cat /proc/sys/kernel/random/uuid'
 alias nano=micro
 
 eval "$(starship init bash)"
