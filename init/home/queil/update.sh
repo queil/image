995e9ac8d995e7e2c8103c45f5860f50f3be2a1e
queil <queil@users.noreply.github.com>
2024-04-13 19:12:58 +0000

    fix

--- init/home/queil/update.sh
+++ init/home/queil/update.sh
@@ -1,18 +1,23 @@
 cd $(git rev-parse --show-toplevel)
 git pull
 
+# vscode-server update
+
+export SERVER_BIN_DIR="$HOME/.vscode-server/bin"
 export VSCODE_EXTRACTED_PATH="/tmp/vscode"
 export VSCODE_TAR_GZ="/tmp/vscode-server-linux-x64.tar.gz"
 
-curl -sSL https://update.code.visualstudio.com/latest/server-linux-x64/stable -o $VSCODE_TAR_GZ
+mkdir -p $SERVER_BIN_DIR
 mkdir -p $VSCODE_EXTRACTED_PATH
+
+curl -sSL https://update.code.visualstudio.com/latest/server-linux-x64/stable -o $VSCODE_TAR_GZ
+
 tar -zxvf $VSCODE_TAR_GZ -C $VSCODE_EXTRACTED_PATH --strip 1
 export VSCODE_GIT_HASH=$(cat "$VSCODE_EXTRACTED_PATH/product.json" | jq -r .commit)
 echo "VSCODE_GIT_HASH: $VSCODE_GIT_HASH"
-export SERVER_BIN_DIR="$HOME/.vscode-server/bin"
-mkdir -p $SERVER_BIN_DIR
+
 export CURRENT_VER_DIR="$SERVER_BIN_DIR/$VSCODE_GIT_HASH"
-mv "$VSCODE_EXTRACTED_PATH" "$SERVER_BIN_DIR"
+mv "$VSCODE_EXTRACTED_PATH" "$CURRENT_VER_DIR"
 touch $CURRENT_VER_DIR/0
 rm $VSCODE_TAR_GZ
 
