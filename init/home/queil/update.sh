2f3101560784a5152fdcb17e1fd1e68b1a8fba83
queil <queil@users.noreply.github.com>
2024-04-13 18:49:48 +0000

    tweaks

--- init/home/queil/update.sh
+++ init/home/queil/update.sh
@@ -3,14 +3,15 @@ git pull
 
 export VSCODE_TAR_GZ=/tmp/vscode-server-linux-x64.tar.gz
 
-curl -sSL https://update.code.visualstudio.com/latest/server-linux-x64/stable -o $VSCODE_TAR_GZ && \
-    tar -zxvf $VSCODE_TAR_GZ -C /tmp && \
-    export VSCODE_GIT_HASH=$(cat /tmp/vscode/product.json | jq -r .commit) && \
-    export BIN_DIR=.vscode-server/bin/$VSCODE_GIT_HASH && \
-    mkdir -p $BIN_DIR && \
-    tar -zxvf /tmp/vscode-server-linux-x64.tar.gz -C $BIN_DIR --strip 1 && \
-    touch $BIN_DIR/0 && \
-    rm $VSCODE_TAR_GZ
+curl -sSL https://update.code.visualstudio.com/latest/server-linux-x64/stable -o $VSCODE_TAR_GZ
+tar -zxvf $VSCODE_TAR_GZ -C /tmp
+export VSCODE_GIT_HASH=$(cat /tmp/vscode/product.json | jq -r .commit)
+echo "VSCODE_GIT_HASH: $VSCODE_GIT_HASH"
+export BIN_DIR=.vscode-server/bin/$VSCODE_GIT_HASH
+mkdir -p $BIN_DIR
+tar -zxvf /tmp/vscode-server-linux-x64.tar.gz -C $BIN_DIR --strip 1
+touch $BIN_DIR/0
+rm $VSCODE_TAR_GZ
 
 cp ./init$HOME -R /home/
 
