a01223ec3585b184fe61b6138450b995f0ae46cd
queil <queil@users.noreply.github.com>
2024-04-13 19:02:13 +0000

    fix

--- init/home/queil/update.sh
+++ init/home/queil/update.sh
@@ -6,7 +6,7 @@ export VSCODE_TAR_GZ="/tmp/vscode-server-linux-x64.tar.gz"
 
 curl -sSL https://update.code.visualstudio.com/latest/server-linux-x64/stable -o $VSCODE_TAR_GZ
 tar -zxvf $VSCODE_TAR_GZ -C $VSCODE_EXTRACTED_PATH --strip 1
-export VSCODE_GIT_HASH=$(cat /tmp/vscode-server-linux-x64/product.json | jq -r .commit)
+export VSCODE_GIT_HASH=$(cat "$VSCODE_EXTRACTED_PATH/product.json" | jq -r .commit)
 echo "VSCODE_GIT_HASH: $VSCODE_GIT_HASH"
 export BIN_DIR="$HOME/.vscode-server/bin/$VSCODE_GIT_HASH"
 mkdir -p $BIN_DIR
