7d6ce04191cf965c991f40b4efa83e74865b6c04
queil <queil@users.noreply.github.com>
2024-05-02 18:49:43 +0000

    add: kustomize preview

--- init/home/queil/kbd.sh
+++ init/home/queil/kbd.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-if [ ! -d /tmp/kb ]; then 
-  mkdir -p /tmp/kb
-fi
+kust_dir=$1
+src_root=$(git rev-parse --show-toplevel)
+repo_name=$(basename "$src_root")
+copy_root="/tmp/kbd/$repo_name"
+ref_dir="$copy_root/$kust_dir"
+ref_file="$ref_dir/ref.yaml"
+mod_file="$ref_dir/mod.yaml"
+mod_dir="$src_root/$kust_dir"
 
-if [ ! -f /tmp/kb/pre.yaml ]; then
-    git stash -u && \
-    kustomize build > /tmp/kb/pre.yaml && \
-    git stash pop
-fi
+echo "src_root: $src_root"
+echo "repo_name: $repo_name"
+echo "copy_root: $copy_root"
+echo "ref_dir: $ref_dir"
+echo "mod_dir: $mod_dir" 
+echo "ref_file: $ref_file"
+echo "mod_file: $mod_file"
 
-if [ -f /tmp/kb/post.yaml ]; then
-  rm /tmp/kb/post.yaml
-fi
+rm $copy_root -rf
+mkdir -p $ref_dir
 
-kustomize build > /tmp/kb/post.yaml && git diff --no-index /tmp/kb/pre.yaml /tmp/kb/post.yaml
+git -C "$src_root" archive HEAD | tar -x -C "$copy_root"
+
+echo "Building ref: $ref_dir -> $ref_file"
+kustomize build "$ref_dir" > $ref_file
+echo "Building mod: $mod_dir -> $mod_file"
+kustomize build "$mod_dir" > $mod_file
+echo "Delta"
+
+delta $ref_file $mod_file
